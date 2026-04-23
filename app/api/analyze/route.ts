b6ad53df1f0433c7182cad5733e1180a7191cc3f
Shunan <161079907+ShunanH@users.noreply.github.com>
2026-04-23 17:09:10 +0000

    redit

--- app/api/analyze/route.ts
+++ app/api/analyze/route.ts
@@ -7,11 +7,12 @@ import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import { NextResponse } from "next/server"
 const redis = new Redis({
-  url: process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL || "",
-  token: process.env.KV_REST_API_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN || "",
+  url: process.env.KV_REST_API_URL || "",
+  token: process.env.KV_REST_API_TOKEN || "",
 });
 
-const ratelimit = new Ratelimit({  redis: redis,
+const ratelimit = new Ratelimit({
+  redis: redis,
   limiter: Ratelimit.slidingWindow(20, "24 h"),
 });
 
